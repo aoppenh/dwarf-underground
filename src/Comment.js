249119fc485923c7134b1956034a17ff61abcc52
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-14 10:16:28 -0400

    added comments box

--- src/Comment.js
+++ src/Comment.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 import './App.css';
+import './Comments.css';
 
 class Comment extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            comment: '',
+        }
+
+        this.addComment = this.addComment.bind(this)
+    }
+
+    addComment(ev) {
+        ev.preventDefault()
+
+        this.setState({
+            comment: ev.target.value,
+        })
+    }
+
     render() {
         return (
             <div className="article-links">
-                <textarea placeholder="Leave a Comment "></textarea>
-                <a className="article-link" href="#">
+                <textarea
+                    placeholder="Leave a Comment"
+                    value={this.state.comment}
+                    onChange={this.addComment}
+                >
+                </textarea>
+                <a className="article-link" href="#" onClick={this.addComment}>
                     <i className="fa fa-comment-o"></i>
                     <span className="article-link-text">Submit</span>
                 </a>
